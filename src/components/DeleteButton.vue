aab1f36d1ecfa81d99e4d99f096268ece9390e46
tharinfernando <tharinwtk1@gmail.com>
2023-05-28 22:43:24 +0530

    add delete button

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="ml-10">
+        <button class="bg-red-500 hover:bg-red-600 w-24 h-10 text-white rounded active:bg-red-700 ">
+            {{ text }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DeleteButton",
+    props: {
+        text: String,
+    },
+}
+</script>
